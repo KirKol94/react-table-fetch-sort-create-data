32f624438452ba9bf1ca7fa87d6b2aba26f386ce
kirkol <kirkol94@ya.ru>
2023-11-28 18:28:36 +0500

    убрал лишний лог

--- src/utils/saveDataToLS.ts
+++ src/utils/saveDataToLS.ts
@@ -5,7 +5,6 @@ export const saveDataToLS = <T>(data: T, keyName: string) => {
       keyName,
       typeof data === "string" ? data : JSON.stringify(data)
     );
-    console.info(`${keyName} data saved to localStorage.`);
   } catch (error) {
     console.error(
       "Error saving data to localStorage:",
