8eea2f4e138e70f6dde516c565f86a0bf352627e
kirkol <kirkol94@ya.ru>
2023-11-28 22:05:55 +0500

    добавил вывод mobx экшенов в логи в режиме разработки

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,19 @@ import ReactDOM from "react-dom/client";
 import { App } from "@/components/app/App";
 import "@/styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
+import { spy } from "mobx";
+import { StrictMode } from "react";
+
+if (process.env.NODE_ENV === "development") {
+  spy((e) => {
+    e.type === "action" && console.log(e);
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </React.StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
