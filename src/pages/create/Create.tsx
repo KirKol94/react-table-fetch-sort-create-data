d0f0223e1fd809151b1a91168d247314226fc4ef
kirkol <kirkol94@ya.ru>
2023-11-28 11:32:11 +0500

    рефакт формы

--- src/pages/create/Create.tsx
+++ src/pages/create/Create.tsx
@@ -4,58 +4,28 @@ import { Button } from "@/components/button";
 import clsx from "clsx";
 import { observer } from "mobx-react-lite";
 import { useCreate } from "./useCreate";
-import { useState, useEffect } from "react";
 
 const Create = observer(() => {
-  const { formData, handleChange, handleSubmit } = useCreate();
-
-  const [formCompleted, setFormCompleted] = useState(false);
-
-  useEffect(() => {
-    const isFormCompleted =
-      formData.birth_year !== "" &&
-      formData.created !== "" &&
-      formData.eye_color !== "" &&
-      formData.gender !== null &&
-      formData.name !== "";
-    setFormCompleted(isFormCompleted);
-  }, [formData]);
+  const { formData, handleChange, handleSubmit, isFormCompleted, inputsData } =
+    useCreate();
 
   return (
     <div className={clsx(classes.create, "create__container")}>
       <h1 className={classes.title}>Create new people</h1>
 
       <form onSubmit={handleSubmit} className={classes.form}>
-        <Input
-          required
-          name="birth_year"
-          placeholder="Birth year 1"
-          value={formData.birth_year}
-          onChange={handleChange}
-        />
-        <Input
-          required
-          name="eye_color"
-          placeholder="Eye color"
-          value={formData.eye_color}
-          onChange={handleChange}
-        />
-        <Input
-          required
-          name="gender"
-          placeholder="Gender"
-          value={formData.gender}
-          onChange={handleChange}
-        />
-        <Input
-          required
-          name="name"
-          placeholder="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-        <Input placeholder={new Date().toString()} disabled />
-        <Button type="submit" disabled={!formCompleted}>
+        {inputsData.map((input) => (
+          <Input
+            key={input.key}
+            name={input.key}
+            placeholder={input.placeHolder}
+            value={input.value}
+            onChange={handleChange}
+          />
+        ))}
+
+        <Input placeholder={formData.created} disabled />
+        <Button type="submit" disabled={!isFormCompleted}>
           Отправить
         </Button>
       </form>
--- src/pages/create/useCreate.ts
+++ src/pages/create/useCreate.ts
@@ -3,10 +3,18 @@ import peoplesStore from "@/store/peoplesStore";
 import { IPeople } from "@/types/people-list";
 import { useState, ChangeEvent, FormEvent } from "react";
 
+interface IInputData {
+  key: keyof IPeople;
+  value: string;
+  placeHolder: string;
+}
+
 interface ICreateDate {
   formData: IPeople;
   handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
   handleSubmit: (e: FormEvent) => void;
+  isFormCompleted: boolean;
+  inputsData: IInputData[];
 }
 
 export const useCreate = (): ICreateDate => {
@@ -49,5 +57,18 @@ export const useCreate = (): ICreateDate => {
     navigate("/");
   };
 
-  return { formData, handleChange, handleSubmit };
+  const inputsData: IInputData[] = [
+    {
+      key: "birth_year",
+      value: formData.birth_year,
+      placeHolder: "Birth year",
+    },
+    { key: "eye_color", value: formData.eye_color, placeHolder: "Eye color" },
+    { key: "gender", value: formData.gender, placeHolder: "Gender" },
+    { key: "name", value: formData.name, placeHolder: "Name" },
+  ];
+
+  const isFormCompleted = !inputsData.some((input) => input.value === "");
+
+  return { formData, handleChange, handleSubmit, isFormCompleted, inputsData };
 };
