e415b5ceaf1c1fa73830a005730c3d2e38c5f5ae
kirkol <kirkol94@ya.ru>
2023-11-29 13:58:37 +0500

    вынес базовый апи url в константу

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,11 @@
+import { BASE_API_URL } from "@/consts/baseApiUrl";
 import { IResponse } from "@/types/people-list";
 import ky from "ky";
 
 class Api {
   async getData(page: number | null | undefined = 1): Promise<IResponse> {
     try {
-      return await ky.get(`https://swapi.dev/api/people/?page=${page}`).json();
+      return await ky.get(`${BASE_API_URL}?page=${page}`).json();
     } catch (error) {
       console.error("Error fetching data:", error);
       throw new Error("Failed to fetch data");
