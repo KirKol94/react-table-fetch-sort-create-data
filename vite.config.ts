dde9408dac10577e27e71bb53ba4aed83ae0e40c
kirkol <kirkol94@ya.ru>
2023-11-29 10:28:55 +0500

    автозапуск страницы в дев режиме

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
@@ -10,4 +8,7 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  server: {
+    open: "./index.html",
+  },
 });
